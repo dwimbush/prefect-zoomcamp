9bf6a66364074735165ff5304b8419b16e88e438
Damien Wimbush <damien.wimbush@gmail.com>
2023-02-04 13:46:34 -0500

    added access token

--- flows/02_gcp/etl_web_to_gcs_HW4.py
+++ flows/02_gcp/etl_web_to_gcs_HW4.py
@@ -9,7 +9,7 @@ github_block = GitHub.load("github-etl-web-to-gcs")
 flow.storage = GitHub(
     repo="prefect-zoomcamp",           # name of repo
     path="flows/02_gcp/etl_web_to_gcs_HW4.py"  # location of flow file in repo
-    # access_token_secret="GITHUB_ACCESS_TOKEN"   # name of personal access token secret
+    access_token_secret="damien1974"   # name of personal access token secret
 )
 
 @task(retries=3)
